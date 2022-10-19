1aebaf329fd2ce739e5f58f0871b5aebd3e6883b
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2022-10-19 11:23:48 +0300

    init

---
+++ android/app/src/main/kotlin/edu/eskisehir/firstapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.eskisehir.firstapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:firstapp/basic_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'immutable_widget.dart';
+
+void main () => runApp(const StaticApp());
+
+class StaticApp extends StatelessWidget{
+  const StaticApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const MaterialApp(
+      home: BasicScreen(),
+    );
+  }
+  
+}
